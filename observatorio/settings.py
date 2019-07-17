4707e2ac63d19b67ffdc88da0f04cb8bf8aff8d3
Lady <ladytrejos@utp.edu.co>
2019-07-17 00:16:01 -0500

    settings

--- observatorio/settings.py
+++ observatorio/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')	
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
