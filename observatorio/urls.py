ea55252985b1f36e69185e9911fec5619e59aadc
Lady <ladytrejos@utp.edu.co>
2019-07-16 23:22:46 -0500

    no media root

--- observatorio/urls.py
+++ observatorio/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    ]
