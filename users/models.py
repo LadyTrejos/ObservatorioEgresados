97a0b8f8defbd0d43194f313779ae27ec4b84faf
Lady <ladytrejos@utp.edu.co>
2019-07-17 22:08:42 -0500

    subir archivos a aws

--- users/models.py
+++ users/models.py
@@ -163,7 +163,7 @@ class Evento(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='admin')
     interests = models.ManyToManyField(Interes, related_name='insterests',blank=True)
-    url = models.ImageField(upload_to="event_img")
+    url = models.ImageField(upload_to="heroku_events_imgs")
 
     def __str__(self):
         return self.name
